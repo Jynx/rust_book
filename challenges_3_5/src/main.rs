653041926b57fff802b0b2b1a20e975f389684dc
steve <sevenex@gmail.com>
2019-08-26 23:44:48 -0500

    add proper input validation for temperature unit

--- challenges_3_5/src/main.rs
+++ challenges_3_5/src/main.rs
@@ -1,23 +1,27 @@
 use std::io;
 
 fn main() {
-    let mut temperature_unit = String::new();
     let mut number_to_convert = String::new();
 
     loop {
+        let mut temperature_unit = String::new();
         println!("Please enter the temperature unit: C or F");
 
         io::stdin()
             .read_line(&mut temperature_unit)
             .expect("failed to read input");
-        
-        let temperature_unit = temperature_unit.to_uppercase();
 
-        //compare inptu strings
-        match temperature_unit.cmp(&)
+        let temperature_unit = temperature_unit.trim().to_uppercase();
 
-        if (temperature_unit != 'C' || temperature_unit != 'F') {
-            printf!("You did not enter a valid temperature Unit ");
+        if (temperature_unit.len() > 2) {
+            println!("you did not enter a valid temperature unit");
+        } 
+
+        let unit_is_celcius = temperature_unit.starts_with('C');
+        let unit_is_farenheit = temperature_unit.starts_with('F');
+
+        if !unit_is_celcius || !unit_is_farenheit {
+            println!("you did not enter a valid temperature unit");
             continue;
         }
     }
@@ -34,6 +38,10 @@ fn main() {
             panic!("You did not enter a valid number");
         }
     };
+
+    if (unit_is_celcius) {
+        let calculated_value: f32 = number_to_convert * 1.8 + 32;
+    }
 }
 
-fn get_valid_user_input() -> i32 {}
+// fn get_valid_user_input() -> i32 {}
