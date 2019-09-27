3a404830ac8e765f9b61c62e8107cf79018f90ad
steve <sevenex@gmail.com>
2019-09-27 18:31:19 -0500

    add associated function

--- structs_example/src/main.rs
+++ structs_example/src/main.rs
@@ -12,6 +12,11 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    //associated function
+    fn create_square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -24,6 +29,8 @@ fn main() {
         height: 20,
         width: 30,
     };
+
+    let square1 = Rectangle::create_square(10);
 
     // println!("rectangle is {:#?}", rectangle);
 
