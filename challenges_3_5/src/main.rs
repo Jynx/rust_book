502f3032e77f9ebe16141caa552a841fbf92802e
steve <sevenex@gmail.com>
2019-09-27 11:51:11 -0500

    remove verbosity and unneeded types that could be inferred

--- challenges_3_5/src/main.rs
+++ challenges_3_5/src/main.rs
@@ -1,63 +1,63 @@
 use std::io;
 
 fn main() {
-    let mut temperature_unit: std::string::String = String::new();
+    let mut temp_unit = String::new();
 
-    query_user_for_temperature_unit(&mut temperature_unit);
-    let value_to_convert: i32 = query_user_for_value_to_convert();
-    let converted_value = convert_user_input(temperature_unit, value_to_convert).to_string();
-    println!("converted Value is {}", converted_value);
+    query_for_temp_unit(&mut temp_unit);
+    let val_to_convert = query_user_for_val();
+    let converted_val = convert_temp_val(temp_unit, val_to_convert).to_string();
+    println!("converted Value is {}", converted_val);
 }
 
-fn query_user_for_temperature_unit(temp_unit_input_buf: &mut std::string::String) {
+fn query_for_temp_unit(unit_input_buf: &mut String) {
     loop {
         println!("Please enter the temperature unit: C or F");
 
         io::stdin()
-            .read_line(temp_unit_input_buf)
+            .read_line(unit_input_buf)
             .expect("failed to read input");
 
-        let temperature_unit = temp_unit_input_buf.trim().to_uppercase();
-        let is_valid_temp_unit = is_valid_temperature_unit(&temperature_unit);
+        let temp_unit = unit_input_buf.trim().to_uppercase();
+        let is_valid_temp_unit = is_valid_temp_unit(&temp_unit);
 
         if !is_valid_temp_unit {
             println!("you did not enter a valid temperature unit");
-            temp_unit_input_buf.clear();
+            unit_input_buf.clear();
             continue;
         }
-        temp_unit_input_buf.clear();
-        temp_unit_input_buf.push_str(&temperature_unit);
+        unit_input_buf.clear();
+        unit_input_buf.push_str(&temp_unit);
         break;
     }
 }
 
-fn query_user_for_value_to_convert() -> i32 {
-    let mut input_buffer: std::string::String = String::new();
+fn query_user_for_val() -> i32 {
+    let mut input_buf = String::new();
     loop {
         println!("Please enter the value to convert");
 
         io::stdin()
-            .read_line(&mut input_buffer)
+            .read_line(&mut input_buf)
             .expect("failed to read input");
 
-        match input_buffer.trim().parse() {
+        match input_buf.trim().parse() {
             Ok(num) => {
                 return num;
             }
             Err(_) => {
                 println!("You did not enter a valid number");
-                input_buffer.clear();
+                input_buf.clear();
                 continue;
             }
         };
     }
 }
 
-fn is_valid_temperature_unit(user_input: &std::string::String) -> bool {
+fn is_valid_temp_unit(user_input: &String) -> bool {
     return user_input.len() == 1 && user_input.starts_with('C') || user_input.starts_with('F');
 }
 
-fn convert_user_input(temp_unit: std::string::String, val: i32) -> f32 {
+fn convert_temp_val(temp_unit: String, val: i32) -> f32 {
     return if temp_unit.starts_with('F') {
         val as f32 * 1.8 + 32.0
     } else {
